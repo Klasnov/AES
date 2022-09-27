4f5fa959d89c50be24178ee40c17b4d3f3c373a4
Klasnov <2654403080@qq.com>
2022-09-27 17:07:20 +0800

    Finished the S box's code.

--- src/extKey.java
+++ src/extKey.java
@@ -9,11 +9,18 @@ import java.util.ArrayList;
  */
 
 public class extKey {
-    private final ArrayList<byte[]> key = new ArrayList<>();
+    private final int nk = 4;
+    private final int wn = 44;
+    private final byte[][] key = new byte[wn][nk];
+    private final int[] rndCst = new int[] {
+            0x01000000, 0x02000000,
+            0x04000000, 0x08000000,
+            0x10000000, 0x20000000,
+            0x40000000, 0x80000000,
+            0x1b000000, 0x36000000
+    };
 
     public extKey(@NotNull String str) {
-        final int nk = 4;
-        final int wn = 44;
         // Parse the key seed from the input string
         byte[] seed = str.getBytes(StandardCharsets.UTF_8);
         // Initialize the W[0] to W[3]
@@ -22,18 +29,22 @@ public class extKey {
         for (cnt = 0; cnt < nk; cnt++) {
             tmpIdx = cnt * 4;
             tmpAry = new byte[] {seed[tmpIdx], seed[tmpIdx + 1], seed[tmpIdx + 2], seed[tmpIdx + 3]};
-            key.add(tmpAry);
+            key[cnt] = tmpAry;
         }
         // Calculate the rest W[i]
     }
 
-    // Converts the input seed characters to the standard key format
-    static public int fmtKey(byte[] k) {
+    private int keyToHex(byte[] k) {
         return (k[0] << 24) + (k[1] << 16) + (k[2] << 8) + k[3];
     }
 
+    private byte[] keyToByte(int key) {
+        byte b1, b2, b3, b4;
+        return new byte[] {};
+    }
+
     // Get the keys of AES
-    public ArrayList<byte[]> getKey() {
+    public byte[][] getKey() {
         return key;
     }
 }
